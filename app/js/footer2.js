bcbc747a09312578eea194158bbc1e9e2802f5a0
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2020-04-20 11:21:23 +0800

    Update footer2.js

--- app/js/footer2.js
+++ app/js/footer2.js
@@ -1,1 +1,1 @@
-﻿document.write("<div id='footer'><img style='width:100%;height:120px;' src='../images/footerwap.jpg'></div>");
+﻿document.write("<div id='footer'><img style='width:100%;height:120px;' src='../3/app/images/footerwap.jpg'></div>");
